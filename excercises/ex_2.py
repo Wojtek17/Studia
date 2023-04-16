0908478f7e501035dc1b1aae65ef4bf20638de08
Wojtek Marszałek <wojtekmarszalek246@gmail.com>
2023-04-16 17:53:56 +0200

    trapez

--- excercises/ex_2.py
+++ excercises/ex_2.py
@@ -8,3 +8,15 @@ pole=1/2*(a*h)
 print(pole)
 obwod=a+b+c 
 print("Pole trojkata wynosi " +str(pole) + " a obwod " +str(obwod))
+
+#trapez
+a_tr=15
+b_tr=4
+c_tr=3
+d_tr=4
+h_tr=10
+pole_trapez=1/2*(a_tr+b_tr)*h_tr
+obw_trapez=a_tr+b_tr+c_tr+d_tr
+print("Pole trapezu wynosi " + str(pole_trapez) + " a obwod wynosi " + str(obw_trapez))
+
+
