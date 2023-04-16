04fca499aa30f59a4a43a732b7027ccbc811d834
Wojtek Marszałek <wojtekmarszalek246@gmail.com>
2023-04-16 16:47:45 +0200

    added task one

--- excercises/ex_1.py
+++ excercises/ex_1.py
@@ -1,1 +1,12 @@
-print("Hello")
+def hello(a: str) -> str:
+    # 
+    return "Hello"
+
+
+print("Example")
+print(hello("Ola"))
+
+assert hello("Wojtek") == "Hello Wojtek"
+assert hello("Zuzia") == "Hello Zuzia"
+
+print("Zadanie zrobione")
