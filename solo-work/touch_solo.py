2f966ed603a63fc106db58034e58385be36c1486
Wojtek Marszałek <wojtekmarszalek246@gmail.com>
2023-05-20 18:06:03 +0200

    Zadanie 1.4

--- solo-work/touch_solo.py
+++ solo-work/touch_solo.py
@@ -6,3 +6,13 @@ print(hello,name)
 #Zadanie 1.2
 name=input("Wpisz imie")
 print("Hello ",name)
+
+#Zadanie 1.3
+studenci = ["Ania", "Kuba", "Piotr", "Jan"]
+liczba_studentow=len(studenci)
+print("Liczba studentow wynosi: ",liczba_studentow)
+
+#Zadanie 1.4
+studenci = ["Ania", "Kasia", "Piotr", "Tomek"]
+for student in studenci:
+    print("Hello ",student)
